dc7ac9c398d21ba1baa37e69e772d23bbf5492da
miniJmo <jmol20student.aau.dk>
2023-11-10 09:41:42 +0100

    made building class file

---
+++ CentralHub.Api.Model/Building.cs
@@ -0,0 +1,39 @@
+using System.Collections.ObjectModel;
+using System.Text.Json.Serialization;
+
+namespace CentralHub.Api.Model;
+
+public sealed class Building
+{
+    [Obsolete("Only for Deserialization")]
+    public Building()
+    {
+    }
+
+    public Building(string name, string description)
+#pragma warning disable CS0618 // Type or member is obsolete
+        : this(name, description, new List<Room>())
+#pragma warning restore CS0618 // Type or member is obsolete
+    {
+    }
+
+    [Obsolete("Only for use with EF Core")]
+    public Building(string name, string description, ICollection<Room> rooms)
+    {
+        Name = name;
+        Description = description;
+        Rooms = rooms;
+    }
+
+    public int BuildingId { get; set; }
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    /// <summary>
+    /// Gets a collection of all rooms in the room.
+    /// </summary>
+    [JsonIgnore]
+    public ICollection<Room> Rooms { get; }
+}
