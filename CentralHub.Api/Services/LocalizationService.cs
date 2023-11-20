bc275e2367ab9eb1643be6be9ff23389436a878b
mast3r <thomasmollerjensen@live.dk>
2023-11-20 10:08:44 +0100

    implemented feature, test fails as the timestamping is not yet implemented on master

--- CentralHub.Api/Services/LocalizationService.cs
+++ CentralHub.Api/Services/LocalizationService.cs
@@ -4,8 +4,14 @@ namespace CentralHub.Api.Services;
 
 public class LocalizationService : ILocalizationService
 {
+    private Thread MeasurementRemover;
     private Dictionary<int, List<Measurement>> _measurements = new Dictionary<int, List<Measurement>>();
 
+    public LocalizationService()
+    {
+        MeasurementRemover = new Thread(new ThreadStart(RemoveMeasurements));
+        MeasurementRemover.Start();
+    }
     public void AddMeasurements(int id, IReadOnlyCollection<Measurement> measurements)
     {
         lock (_measurements)
@@ -17,6 +23,28 @@ public class LocalizationService : ILocalizationService
             else
             {
                 _measurements.Add(id, measurements.ToList());
+            }
+        }
+    }
+    
+    private void RemoveMeasurements()
+    {
+        while (true)
+        {
+            if (_measurements.Count == 0)
+            {
+                break;
+            }
+
+            foreach (var key in _measurements.Keys)
+            {
+                foreach (var measurement in _measurements[key])
+                {
+                    if (DateTime.Now >= DateTime.Now + TimeSpan.FromMinutes(2))
+                    { // TODO: Change second datetime.now to datetime from addMeasurements when it has been implemented
+                        _measurements[key].Remove(measurement);
+                    }
+                }
             }
         }
     }
