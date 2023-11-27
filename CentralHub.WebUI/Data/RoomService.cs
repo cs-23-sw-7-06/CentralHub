41311af5c2af24f34fa06ce4147334aa8b2d8b07
thom776g <thomas@nordfiber.dk>
2023-11-27 15:45:59 +0100

    Implement UI

--- CentralHub.WebUI/Data/RoomService.cs
+++ CentralHub.WebUI/Data/RoomService.cs
@@ -1,7 +1,12 @@
 using System.Net;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
 using CentralHub.Api.Model;
+using CentralHub.Api.Model.Requests.Room;
 using CentralHub.Api.Model.Responses.Room;
 using CentralHub.Api.Model.Responses.Tracker;
+using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace CentralHub.WebUI.Data;
 
@@ -18,7 +23,7 @@ public sealed class RoomService
     {
         var request = new HttpRequestMessage(
             HttpMethod.Delete,
-            $"http://localhost:8081/room/remove?id={room.RoomId}");
+            $"http://localhost:8081/room/remove?roomId={room.RoomId}");
         request.Headers.Add("Accept", "application/json");
         request.Headers.Add("User-Agent", "CentralHub.WebUI");
 
@@ -31,6 +36,50 @@ public sealed class RoomService
             throw new InvalidOperationException("Somethings fucky");
         }
     }
+
+    public async Task UpdateRoomAsync(Room room, String name, String description, CancellationToken cancellationToken)
+    {
+        var updateRoomRequest = new UpdateRoomRequest(room.RoomId, name, description);
+        var request = new HttpRequestMessage(
+            HttpMethod.Put,
+            $"http://localhost:8081/room/update"
+            );
+        request.Headers.Add("Accept", "application/json");
+        request.Headers.Add("User-Agent", "CentralHub.WebUI");
+        request.Content = new StringContent(JsonSerializer.Serialize(updateRoomRequest), Encoding.UTF8, "application/json");
+
+        var client = _clientFactory.CreateClient();
+
+        var response = await client.SendAsync(request, cancellationToken);
+
+        if (response.StatusCode != HttpStatusCode.OK)
+        {
+            throw new InvalidOperationException("Somethings fucky");
+        }
+    }
+
+    public async Task AddRoomAsync(String name, String description, CancellationToken cancellationToken)
+    {
+        var addRoomRequest = new AddRoomRequest(name, description);
+        var request = new HttpRequestMessage(
+            HttpMethod.Post,
+            $"http://localhost:8081/room/add"
+            );
+        request.Headers.Add("Accept", "application/json");
+        request.Headers.Add("User-Agent", "CentralHub.WebUI");
+        request.Content = new StringContent(JsonSerializer.Serialize(addRoomRequest), Encoding.UTF8, "application/json");
+
+        var client = _clientFactory.CreateClient();
+
+        var response = await client.SendAsync(request, cancellationToken);
+
+        if (response.StatusCode != HttpStatusCode.OK)
+        {
+            throw new InvalidOperationException("Somethings fucky");
+        }
+    }
+
+
 
     public async Task<Room[]> GetRoomsAsync(CancellationToken cancellationToken)
     {
--- CentralHub.WebUI/Data/TrackerService.cs
+++ CentralHub.WebUI/Data/TrackerService.cs
@@ -1,6 +1,14 @@
+using System.Net;
+using System.Text.Json;
 using System.Collections.Immutable;
 using CentralHub.Api.Model.Responses.Room;
 using CentralHub.Api.Model.Responses.Tracker;
+using CentralHub.Api.Model.Requests.Tracker;
+using System.Net.Http.Headers;
+using System.Text;
+using CentralHub.Api.Model;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using CentralHub.Api.Model.Requests;
 
 namespace CentralHub.WebUI.Data;
 
@@ -11,6 +19,76 @@ public sealed class TrackerService
     public TrackerService(IHttpClientFactory clientFactory)
     {
         _clientFactory = clientFactory;
+    }
+
+    public async Task RemoveTrackerAsync(Tracker tracker, CancellationToken cancellationToken)
+    {
+        var request = new HttpRequestMessage(
+            HttpMethod.Delete,
+            $"http://localhost:8081/tracker/remove?trackerId={tracker.TrackerId}");
+        request.Headers.Add("Accept", "application/json");
+        request.Headers.Add("User-Agent", "CentralHub.WebUI");
+
+        var client = _clientFactory.CreateClient();
+
+        var response = await client.SendAsync(request, cancellationToken);
+
+        if (response.StatusCode != HttpStatusCode.OK)
+        {
+            throw new InvalidOperationException("Somethings fucky");
+        }
+    }
+
+    public async Task AddTrackerAsync(Room room, UnregisteredTracker tracker, String name, String description, CancellationToken cancellationToken)
+    {
+        var addTrackerRequest = new AddTrackerRequest(room.RoomId, name, description, tracker.WifiMacAddress, tracker.BluetoothMacAddress);
+        var request = new HttpRequestMessage(
+            HttpMethod.Post,
+            $"http://localhost:8081/tracker/add"
+            );
+        request.Headers.Add("Accept", "application/json");
+        request.Headers.Add("User-Agent", "CentralHub.WebUI");
+        request.Content = new StringContent(JsonSerializer.Serialize(addTrackerRequest), Encoding.UTF8, "application/json");
+
+        var client = _clientFactory.CreateClient();
+
+        var response = await client.SendAsync(request, cancellationToken);
+
+        if (response.StatusCode != HttpStatusCode.OK)
+        {
+            throw new InvalidOperationException("Somethings fucky");
+        }
+    }
+
+    public async Task UpdateTrackerAsync(Room room, Tracker tracker, String name, String description, CancellationToken cancellationToken)
+    {
+        if (room.RoomId == tracker.RoomId)
+        {
+            var addTrackerRequest = new UpdateTrackerRequest(tracker.TrackerId, name, description);
+            var request = new HttpRequestMessage(
+                HttpMethod.Put,
+                $"http://localhost:8081/tracker/update"
+                );
+            request.Headers.Add("Accept", "application/json");
+            request.Headers.Add("User-Agent", "CentralHub.WebUI");
+            request.Content = new StringContent(JsonSerializer.Serialize(addTrackerRequest), Encoding.UTF8, "application/json");
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request, cancellationToken);
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                throw new InvalidOperationException("Somethings fucky");
+            }
+        }
+        else
+        {
+            await RemoveTrackerAsync(tracker, cancellationToken);
+            UnregisteredTracker[] uTrackers = await GetUnregisteredTrackersAsync(cancellationToken);
+            UnregisteredTracker uTracker = uTrackers.Single(t => t.WifiMacAddress == tracker.WifiMacAddress);
+            await AddTrackerAsync(room, uTracker, name, description, cancellationToken);
+        }
     }
 
     public async Task<Tracker[]> GetTrackersAsync(Room room, CancellationToken cancellationToken)
